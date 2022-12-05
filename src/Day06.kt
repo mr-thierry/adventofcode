1fb79ac7be369d6c5e92ba37e7839f90029c61bb
mr-thierry <thierry.roy@aexp.com>
2022-12-05 07:16:58 -0500

    Prepare for Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,40 @@
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class Day06 {
+
+    private val day = "Day05"
+
+    @Test
+    fun testSolution1() {
+        assertThat(solution1(readInput("${day}_test"))).isEqualTo("")
+    }
+
+    @Test
+    fun solution1() {
+        assertThat(solution1(readInput(day))).isEqualTo("")
+    }
+
+    @Test
+    fun testSolution2() {
+        assertThat(solution2(readInput("${day}_test"))).isEqualTo("")
+    }
+
+    @Test
+    fun solution2() {
+        assertThat(solution2(readInput(day))).isEqualTo("")
+    }
+
+    private fun solution1(input: List<String>): String {
+        TODO()
+    }
+
+    private fun solution2(input: List<String>): String {
+        TODO()
+    }
+
+}
+
+
+
