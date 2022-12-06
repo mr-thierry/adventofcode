932d6b2d2f7a00f7d924b74dcc115dcc601350b5
mr-thierry <thierry.roy@aexp.com>
2022-12-06 06:47:08 -0500

    Day 6

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,29 +1,3 @@
-fun main() {
-    val input = readInput("Day02")
-    solution1(input)
-    solution2(input)
-}
-
-fun solution1(input: List<String>) {
-    var score = 0
-    input.forEach {
-        val opponent = it.first()
-        val us = it.last()
-        score += calculateScoreSolution1(opponent, us)
-    }
-    println("Result 1:$score")
-}
-
-fun solution2(input: List<String>) {
-    var score = 0
-    input.forEach {
-        val opponent = it.first()
-        val us = it.last()
-        score += calculateScoreSolution2(opponent, us)
-    }
-    println("Result 2:$score")
-}
-
 private const val WIN = 6
 private const val DRAW = 3
 private const val LOOSE = 0
@@ -32,58 +6,92 @@ private const val SCORE_WITH_ROCK = 1
 private const val SCORE_WITH_PAPER = 2
 private const val SCORE_WITH_SCISSOR = 3
 
-fun calculateScoreSolution1(opponent: Char, us: Char): Int {
-    when (opponent) {
-        'A' -> {
-            return when (us) {
-                'X' -> DRAW + SCORE_WITH_ROCK
-                'Y' -> WIN + SCORE_WITH_PAPER
-                else -> LOOSE + SCORE_WITH_SCISSOR
-            }
-        }
+fun main() {
+    val input = readInput("Day02")
+    Day02().solution1(input)
+    Day02().solution2(input)
+}
 
-        'B' -> {
-            return when (us) {
-                'X' -> LOOSE + SCORE_WITH_ROCK
-                'Y' -> DRAW + SCORE_WITH_PAPER
-                else -> WIN + SCORE_WITH_SCISSOR
-            }
-        }
 
-        else -> {
-            return when (us) {
-                'X' -> WIN + SCORE_WITH_ROCK
-                'Y' -> LOOSE + SCORE_WITH_PAPER
-                else -> DRAW + SCORE_WITH_SCISSOR
+class Day02 {
+
+
+    fun solution1(input: List<String>) {
+        var score = 0
+        input.forEach {
+            val opponent = it.first()
+            val us = it.last()
+            score += calculateScoreSolution1(opponent, us)
+        }
+        println("Result 1:$score")
+    }
+
+    fun solution2(input: List<String>) {
+        var score = 0
+        input.forEach {
+            val opponent = it.first()
+            val us = it.last()
+            score += calculateScoreSolution2(opponent, us)
+        }
+        println("Result 2:$score")
+    }
+
+
+    fun calculateScoreSolution1(opponent: Char, us: Char): Int {
+        when (opponent) {
+            'A' -> {
+                return when (us) {
+                    'X' -> DRAW + SCORE_WITH_ROCK
+                    'Y' -> WIN + SCORE_WITH_PAPER
+                    else -> LOOSE + SCORE_WITH_SCISSOR
+                }
+            }
+
+            'B' -> {
+                return when (us) {
+                    'X' -> LOOSE + SCORE_WITH_ROCK
+                    'Y' -> DRAW + SCORE_WITH_PAPER
+                    else -> WIN + SCORE_WITH_SCISSOR
+                }
+            }
+
+            else -> {
+                return when (us) {
+                    'X' -> WIN + SCORE_WITH_ROCK
+                    'Y' -> LOOSE + SCORE_WITH_PAPER
+                    else -> DRAW + SCORE_WITH_SCISSOR
+                }
             }
         }
     }
-}
 
-fun calculateScoreSolution2(opponent: Char, us: Char): Int {
-    when (opponent) {
-        'A' -> { //ROCK
-            return when (us) {
-                'X' -> LOOSE + SCORE_WITH_SCISSOR
-                'Y' -> DRAW + SCORE_WITH_ROCK
-                else -> WIN + SCORE_WITH_PAPER
+    fun calculateScoreSolution2(opponent: Char, us: Char): Int {
+        when (opponent) {
+            'A' -> { //ROCK
+                return when (us) {
+                    'X' -> LOOSE + SCORE_WITH_SCISSOR
+                    'Y' -> DRAW + SCORE_WITH_ROCK
+                    else -> WIN + SCORE_WITH_PAPER
+                }
             }
-        }
 
-        'B' -> { //PAPER
-            return when (us) {
-                'X' -> LOOSE + SCORE_WITH_ROCK
-                'Y' -> DRAW + SCORE_WITH_PAPER
-                else -> WIN + SCORE_WITH_SCISSOR
+            'B' -> { //PAPER
+                return when (us) {
+                    'X' -> LOOSE + SCORE_WITH_ROCK
+                    'Y' -> DRAW + SCORE_WITH_PAPER
+                    else -> WIN + SCORE_WITH_SCISSOR
+                }
             }
-        }
 
-        else -> { //SCISSOR
-            return when (us) {
-                'X' -> LOOSE + SCORE_WITH_PAPER
-                'Y' -> DRAW + SCORE_WITH_SCISSOR
-                else -> WIN + SCORE_WITH_ROCK
+            else -> { //SCISSOR
+                return when (us) {
+                    'X' -> LOOSE + SCORE_WITH_PAPER
+                    'Y' -> DRAW + SCORE_WITH_SCISSOR
+                    else -> WIN + SCORE_WITH_ROCK
+                }
             }
         }
     }
+
 }
+
--- src/Day06.kt
+++ src/Day06.kt
@@ -8,30 +8,52 @@ class Day06 {
 
     @Test
     fun testSolution1() {
-        assertThat(solution1(readInput("${day}_test"))).isEqualTo("")
+        val strings = readInput("${day}_test")
+        assertThat(solution1(strings[0])).isEqualTo(7)
+        assertThat(solution1(strings[1])).isEqualTo(5)
+        assertThat(solution1(strings[2])).isEqualTo(6)
+        assertThat(solution1(strings[3])).isEqualTo(10)
+        assertThat(solution1(strings[4])).isEqualTo(11)
     }
 
     @Test
     fun solution1() {
-        assertThat(solution1(readInput(day))).isEqualTo("")
+        assertThat(solution1(readInput(day)[0])).isEqualTo(1779)
     }
 
     @Test
     fun testSolution2() {
-        assertThat(solution2(readInput("${day}_test"))).isEqualTo("")
+        val strings = readInput("${day}_test")
+        assertThat(solution2(strings[0])).isEqualTo(19)
+        assertThat(solution2(strings[1])).isEqualTo(23)
+        assertThat(solution2(strings[2])).isEqualTo(23)
+        assertThat(solution2(strings[3])).isEqualTo(29)
+        assertThat(solution2(strings[4])).isEqualTo(26)
     }
 
     @Test
     fun solution2() {
-        assertThat(solution2(readInput(day))).isEqualTo("")
+        assertThat(solution2(readInput(day)[0])).isEqualTo(2635)
     }
 
-    private fun solution1(input: List<String>): String {
-        TODO()
+    private fun solution1(input: String): Int {
+        return findStartOfStream(input, matchCount = 4)
+
     }
 
-    private fun solution2(input: List<String>): String {
-        TODO()
+    private fun solution2(input: String): Int {
+        return findStartOfStream(input, matchCount = 14)
+    }
+
+    private fun findStartOfStream(input: String, matchCount: Int): Int {
+        var index = 0
+        input.toCharArray().toList().windowed(matchCount) { list ->
+            if (index == 0 && list.distinct().size == matchCount) {
+                index = input.indexOf(list.joinToString(""))
+            }
+        }
+
+        return index + matchCount
     }
 
 }
